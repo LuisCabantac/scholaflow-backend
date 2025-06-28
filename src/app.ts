7bb4465770c123af1f4dfb52746ca2389bde80bd
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-29 06:26:32 +0800

    refactor: Set up express app for vercel

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+import userRouter from "./routes/users";
+import classroomsRouter from "./routes/classrooms";
+import accountsController from "./routes/accounts";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/v1/api/users", userRouter);
+
+app.use("/v1/api/accounts", accountsController);
+
+app.use("/v1/api/classrooms", classroomsRouter);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import bodyParser from "body-parser";
+import app from "../src/app";
 
-import userRouter from "./routes/users";
-import classroomsRouter from "./routes/classrooms";
-import accountsController from "./routes/accounts";
+const port = process.env.PORT || 8080;
 
-const app = express();
-const port = 8080;
-
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(bodyParser.json());
-
-app.use("/v1/api/users", userRouter);
-
-app.use("/v1/api/accounts", accountsController);
-
-app.use("/v1/api/classrooms", classroomsRouter);
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+}
